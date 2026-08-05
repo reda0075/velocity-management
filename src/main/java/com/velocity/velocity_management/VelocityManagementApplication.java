4db15cc94070c4cf57a172de1642af41947d8e8f
Reda <elhamdaoui4453@gmail.com>
2026-08-05 17:27:52 +0200

    chore: initialize project architecture

---
+++ src/main/java/com/velocity/velocity_management/VelocityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.velocity.velocity_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelocityManagementApplication.class, args);
+	}
+
+}
